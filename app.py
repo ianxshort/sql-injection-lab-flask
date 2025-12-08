8d37543e043cea5b09d9707cf0b3e7f2ee0ada43
ianxshort <tut42883@temple.edu>
2025-12-08 13:08:43 -0500

    Created a simple database and connected app to sqllite

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,g 
+import sqlite3
 
 app = Flask(__name__)
+DATABASE='users.db'
+
+def get_db():
+    if "db" not in g :
+        g.db=sqlite3.connect(DATABASE) #open a connection so I can run queries 
+    return g.db
 
 @app.route('/')
 def home():
